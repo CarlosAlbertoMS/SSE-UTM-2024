776c0c00e1378fcacd007ca8f10fe28b9d62f46f
DavidMatias44 <daosmaru3667@gmail.com>
2025-02-19 11:00:36 -0600

    Eventos espero que ahora funcione.

--- resources/views/Egresados/Eventos.blade.php
+++ resources/views/Egresados/Eventos.blade.php
@@ -12,7 +12,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    	<script src="{{ asset('js/eventos.js') }}"></script>
+    	{{-- <script src="{{ asset('js/eventos.js') }}"></script> --}}
 
 		<title>Eventos</title>
 	</head>
@@ -20,6 +20,7 @@
 	@include('layouts.egresados.header')
    
 	<main>
+		{{ asset('js/eventos.js') }}
 		<section class="main--section">
 			<div class="main--container--1">
 				<x-buscador></x-buscador>
@@ -29,4 +30,74 @@
 			</div>
 		</section>
 	</main>
+
+
+	<script>
+		document.addEventListener('DOMContentLoaded', function() {
+    		var img_ant = new Image();
+    		img_ant.src = "../assets/icons/Marcador_B.svg";
+
+    		function Marcador_selected(imagen) {
+        		if(img_ant.src === imagen.src){
+            		imagen.src = "../assets/icons/Marcador_BG.svg";
+        		}else{
+            		imagen.src = img_ant.src;
+        		}
+    		}
+
+    		document.querySelector('.main--container--2').addEventListener('click', function(event) {
+        		if (event.target && event.target.classList.contains('btn-ver-detalle')) {
+            		const eventoId = event.target.getAttribute('data-id');
+            		const paginaActual = event.target.getAttribute('data-page');
+
+            		history.pushState({ pagina: paginaActual }, '', `?page=${paginaActual}`);
+            		cargarDetalleEvento(eventoId);
+        		}
+    		});
+
+    		document.querySelector('.main--container--2').addEventListener('click', function(event) {
+        		if (event.target && event.target.id === 'btn-regresar') {
+            		history.back();
+        		}
+    		});
+
+    		window.addEventListener('popstate', function(event) {
+        		const pagina = event.state ? event.state.pagina : 1;
+        		cargarPagina(pagina);
+    		});
+
+    		function cargarDetalleEvento(eventoId) {
+        		$.ajax({
+            		url: `/Egresados/Eventos/${eventoId}`,
+            		method: 'GET',
+            		dataType: 'json',
+            		success: function(response) {
+                		document.querySelector('.main--container--1').style.display = 'none';
+                		$('.main--container--2').html(
+                    		`@include('layouts.egresados.eventos-show')`
+                    		);
+            		},
+            		error: function(error) {
+                		console.error('Error al cargar el detalle del evento:', error);
+            		}
+        		});
+    		}
+
+    		function cargarPagina(pagina) {
+        		$.ajax({
+            		url: `/Egresados/Eventos?page=${pagina}`,
+            		method: 'GET',
+            		success: function(response) {
+                		document.querySelector('.main--container--1').style.display = 'block';
+                		$('.main--container--2').html(
+                    		`@include('layouts.egresados.eventos-index', ['paginador' => $paginador])`
+                		);
+            		},
+            		error: function(error) {
+                		console.error('Error al cargar la página:', error);
+            		}
+        		});
+    		}
+		});
+	</script>
 </html>
